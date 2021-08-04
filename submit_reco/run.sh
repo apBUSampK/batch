a0114eeb1e0fea16a5e42a75e69e5609d7d38b9b
Oleg Golosov <oleg.golosov@gmail.com>
2021-08-04 07:07:41 +0300

    still wip

--- submit_reco/run.sh
+++ submit_reco/run.sh
@@ -1,29 +1,67 @@
 #!/bin/bash
 
 function getJsonVal () {
-  python -c "import json,sys;sys.stdout.write(json.dumps(json.load(open('${config}'))$1))";
-} # e.x. 'out_dir=$(dirname $(getJsonVal "['transport']['output']['path']"))'
+  val=$(python -c "import json;print(json.dumps(json.load(open('${config}'))$1))";)
+  eval echo ${val}
+} # e.x. 'out_path=$(getJsonVal "['transport']['output']['path']")'
 
-function checkJsonKey () {
-  python -c "import json,sys;print ('$1' in json.load(open('${config}')))";
-} # returns True if key exists, False if not, e.x. 'run_transport=$(checkJsonKey "transport")'
+#function checkJsonKey () {
+#  python -c "import json,sys;print ('$1' in json.load(open('${config}')))";
+#} # returns True if key exists, False if not, e.x. 'run_transport=$(checkJsonKey "transport")'
+
+batch=false
 
 config=$1
-
-nEvents=$(getJsonVal "['nEvents']")
-run_transport=$(getJsonVal "['run_transport']")
-run_digitization=$(getJsonVal "['run_digi']")
-run_reconstruction=$(getJsonVal "['run_reco']")
-run_AT=$(getJsonVal "['run_AT']")
+jobName=$(getJsonVal "['accessory']['jobName']")
+jobRange=$(getJsonVal "['accessory']['jobRange']")
+logDir=$(getJsonVal "['accessory']['logDir']")
+jobScript=$(getJsonVal "['accessory']['jobScript']")
+cbmRoot=$(getJsonVal "['accessory']['cbmRoot']")
+source ${cbmRoot}
 
 steps=(transport digitization reconstruction AT)
-for step in $(steps[*]); do
-  run_transport=$(getJsonVal "['run_transport']")
-  if [ run_${step} == true ]; then
-    out_dir=$(dirname $(getJsonVal "['${step}']['output']['path']"))
-    ${step}_macro=$(getJsonVal "['accessory']['${step}_macro']")
-    mkdir -p ${out_dir}
-    rsync -v ${step}_macro ${out_dir}
-    export ${step}_macro=$(basename ${step}_macro)
+declare -A run_steps
+declare -A step_out_dirs
+declare -A step_macros
+for step in ${steps[*]}; do
+  run_step=$(getJsonVal "['accessory']['${step}']['run']")
+  if [ ${run_step} == true ]; then
+    step_out_dir=$(dirname $(getJsonVal "['${step}']['output']['path']"))
+    step_macro=$(getJsonVal "['accessory']['${step}']['macro']")
+    step_src_dir=${step_out_dir}/macro
+    mkdir -pv ${step_out_dir}
+    mkdir -pv ${step_src_dir}
+    cp -v ${step_macro} ${step_src_dir}
+    cp -v ${config} ${step_src_dir}
+    run_steps[${step}]=${run_step}
+    step_out_dirs[${step}]=${step_src_dir}
+    step_macros[${step}]=${step_src_dir}/$(basename ${step_macro})
   fi
 done
+
+export run_transport=${run_steps[transport]}
+export transport_out_dir=${step_out_dirs[transport]}
+export transport_macro=${step_macros[transport]}
+
+export run_digitization=${run_steps[digitization]}
+export digitization_out_dir=${step_out_dirs[digitization]}
+export digitization_macro=${step_macros[digitization]}
+ 
+export run_reconstruction=${run_steps[reconstruction]}
+export reconstruction_out_dir=${step_out_dirs[reconstruction]}
+export reconstruction_macro=${step_macros[reconstruction]}
+
+export run_AT=${run_steps[AT]}
+export AT_out_dir=${step_out_dirs[AT]}
+export AT_macro=${step_macros[AT]}
+
+export nEvents=$(getJsonVal "['accessory']['nEvents']")
+
+if [ ${batch} == true ];then
+  sbatch -J ${jobName} -o ${logDir}/%A_%a.log -a ${jobRange} --export=ALL -- ${jobSript}
+else
+  mkdir -pv ${logDir}
+  export SLURM_ARRAY_JOB_ID=jobId
+  export SLURM_ARRAY_TASK_ID=taskId
+  ./${jobScript}
+fi
--- submit_reco/run_sim_reco.sh
+++ submit_reco/run_sim_reco.sh
@@ -1,50 +1,49 @@
 #!/bin/bash
 
-function getJsonVal () { # e.x. 'out_dir=$(dirname $(getJsonVal "['transport']['output']['path']"))'
-  python -c "import json,sys;sys.stdout.write(json.dumps(json.load(open('${config}'))$1))"; 
-}
+jobId=${SLURM_ARRAY_JOB_ID}
+taskId=${SLURM_ARRAY_TASK_ID}
 
-function checkJsonKey () { # returns True if key exists, False if not, e.x. 'run_transport=$(checkJsonKey "transport")'
-  python -c "import json,sys;print ('$1' in json.load(open('${config}')))"; 
-}
-
-config=$1
-
-nEvents=$(getJsonVal "['accessory']['nEvents']")
-run_transport=$(getJsonVal "['accessory']['run_transport']")
-run_digi=$(getJsonVal "['accessory']['run_digi']")
-run_reco=$(getJsonVal "['accessory']['run_reco']")
-run_AT=$(getJsonVal "['accessory']['run_AT']")
-
-[ -z ${transport_macro} ] && transport_macro=$(getJsonVal "['accessory']['transport_macro']")
-[ -z ${digi_macro} ] && digi_macro=$(getJsonVal "['accessory']['digi_macro']")
-[ -z ${reco_macro} ] && reco_macro=$(getJsonVal "['accessory']['reco_macro']")
-[ -z ${AT_macro} ] && AT_macro=$(getJsonVal "['accessory']['AT_macro']")
-
-[ ${run_transport} == true ] && root -b -l -q "${transport_macro}(\"${config}\",${nEvents})"
-
-[ ${run_digi} == true ] && root -b -l -q "${digi_macro}(\"${config}\",${nEvents})"
-
-if [ ${run_reco} == true ]; then
-  input=$(getJsonVal "['reconstruction']['input']")
-  nTimeSlices=$(getJsonVal "['reconstruction']['nTimeSlices']")
-  firstTimeSlice=$(getJsonVal "['reconstruction']['firstTimeSlice']")
-  outputReco=$(getJsonVal "['reconstruction']['output']['path']")
-  overwriteReco=$(getJsonVal "['reconstruction']['output']['overwrite']")
-  sEvBuildRaw=$(getJsonVal "['reconstruction']['sEvBuildRaw']")
-  paramFile=$(getJsonVal "['reconstruction']['paramFile']")
-  useMC=$(getJsonVal "['reconstruction']['useMC']")
-  root -b -l -q "${reco_macro}(${input},${nTimeSlices},${firstTimeSlice},${outputReco},${overwriteReco},${sEvBuildRaw},\"${config}\",${paramFile},${useMC})"
+if [ ${run_transport} == true ]; then
+  config=$(ls ${transport_out_dir}/*.json) 
+  job_src_dir=$(dirname ${transport_macro})/${jobId}
+  job_transport_macro=${job_src_dir}/$(basename ${transport_macro})
+  job_config=${job_src_dir}/$(basename ${config})
+  mkdir -pv ${job_src_dir}
+  mv -v ${transport_macro} ${job_transport_macro}
+  mv -v ${config} ${job_config}
+  task_dir=${transport_out_dir}/${taskId}
+  job_log_dir=${transport_out_dir}/log_${jobId}
+  log=${job_log_dir}/${taskId}.tra.log
+  mkdir -p ${job_log_dir}
+  mkdir -p ${task_dir}
+  #cd ${task_dir}
+  ln -sf ${VMCWORKDIR}/macro/run/.rootrc .
+  root -b -l -q "${job_transport_macro}(\"${job_config}\",${nEvents})" &> ${log}
+  gzip -f ${log} 
+  rm .rootrc
 fi
 
-if [ ${run_AT} == true ]; then
-  traPath=$(getJsonVal "['AT']['traPath']")
-  rawPath=$(getJsonVal "['AT']['rawPath']")
-  recPath=$(getJsonVal "['AT']['recPath']")
-  geoPath=$(getJsonVal "['AT']['geoPath']")
-  parPath=$(getJsonVal "['AT']['parPath']")
-  unigenFile=$(getJsonVal "['AT']['unigenFile']")
-  outputAT=$(getJsonVal "['AT']['output']['path']")
-  overwriteAT=$(getJsonVal "['AT']['output']['overwrite']")
-  root -b -l -q "${AT_macro}(${traPath},${rawPath},${recPath},${geoPath},${parPath},${unigenFile},${oututAT},${overwriteAT},\"${config}\",${nEvents})"
-fi
+
+#if [ ${run_reconstruction} == true ]; then
+#  input=$(getJsonVal "['reconstruction']['input']")
+#  nTimeSlices=$(getJsonVal "['reconstruction']['nTimeSlices']")
+#  firstTimeSlice=$(getJsonVal "['reconstruction']['firstTimeSlice']")
+#  outputReco=$(getJsonVal "['reconstruction']['output']['path']")
+#  overwriteReco=$(getJsonVal "['reconstruction']['output']['overwrite']")
+#  sEvBuildRaw=$(getJsonVal "['reconstruction']['sEvBuildRaw']")
+#  paramFile=$(getJsonVal "['reconstruction']['paramFile']")
+#  useMC=$(getJsonVal "['reconstruction']['useMC']")
+#  root -b -l -q "${reco_macro}(${input},${nTimeSlices},${firstTimeSlice},${outputReco},${overwriteReco},${sEvBuildRaw},\"${config}\",${paramFile},${useMC})"
+#fi
+#
+#if [ ${run_AT} == true ]; then
+#  traPath=$(getJsonVal "['AT']['traPath']")
+#  rawPath=$(getJsonVal "['AT']['rawPath']")
+#  recPath=$(getJsonVal "['AT']['recPath']")
+#  geoPath=$(getJsonVal "['AT']['geoPath']")
+#  parPath=$(getJsonVal "['AT']['parPath']")
+#  unigenFile=$(getJsonVal "['AT']['unigenFile']")
+#  outputAT=$(getJsonVal "['AT']['output']['path']")
+#  overwriteAT=$(getJsonVal "['AT']['output']['overwrite']")
+#  root -b -l -q "${AT_macro}(${traPath},${rawPath},${recPath},${geoPath},${parPath},${unigenFile},${oututAT},${overwriteAT},\"${config}\",${nEvents})"
+#fi
