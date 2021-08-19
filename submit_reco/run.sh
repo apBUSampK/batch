7759cea5d3feac0a039c09eff5c3e4c19ab9ff2f
Oleg Golosov <oleg.golosov@gmail.com>
2021-08-19 17:33:53 +0300

    working?

--- submit_reco/run.sh
+++ submit_reco/run.sh
@@ -9,66 +9,51 @@ function getJsonVal () {
 #  python -c "import json,sys;print ('$1' in json.load(open('${config}')))";
 #} # returns True if key exists, False if not, e.x. 'run_transport=$(checkJsonKey "transport")'
 
-batch=false
+function read_step_info () {
+  run=$(getJsonVal "['accessory']['${step}']['run']")
+  if [ ${run} == true ]; then
+    out_dir=$(getJsonVal "['${step}']['output']['path']")
+    [[ ${out_dir} != */ ]] && out_dir=$(dirname ${out_dir})
+    src_dir=${out_dir}/macro
+    macro=$(getJsonVal "['accessory']['${step}']['macro']")
+    macro_name=$(basename ${macro})
+  fi
+}
 
 submit_script=${0}
 config=${1}
-jobName=$(getJsonVal "['accessory']['jobName']")
-jobRange=$(getJsonVal "['accessory']['jobRange']")
-logDir=$(getJsonVal "['accessory']['logDir']")
+batch=$(getJsonVal "['accessory']['batch']")
 jobScript=$(getJsonVal "['accessory']['jobScript']")
 cbmRoot=$(getJsonVal "['accessory']['cbmRoot']")
 source ${cbmRoot}
 
-steps=(transport digitization reconstruction AT)
-declare -A run_steps
-declare -A step_out_dirs
-declare -A step_macros
-for step in ${steps[*]}; do
-  run_step=$(getJsonVal "['accessory']['${step}']['run']")
-  if [ ${run_step} == true ]; then
-    step_out_dir=$(dirname $(getJsonVal "['${step}']['output']['path']"))
-    step_macro=$(getJsonVal "['accessory']['${step}']['macro']")
-    step_src_dir=${step_out_dir}/macro
-    mkdir -pv ${step_out_dir}
-    mkdir -pv ${step_src_dir}
-    cp -v ${step_macro} ${step_src_dir}
-    cp -v ${config} ${step_src_dir}
-    cp -v ${submit_script} ${step_src_dir}
-    cp -v ${jobScript} ${step_src_dir}
-    run_steps[${step}]=${run_step}
-    step_out_dirs[${step}]=${step_out_dir}
-    step_macros[${step}]=${step_src_dir}/$(basename ${step_macro})
+steps="transport digitization reconstruction AT"
+for step in ${steps}; do
+  read_step_info
+  if [ ${run} == true ]; then
+    mkdir -pv ${src_dir}
+    cp -v ${macro} ${src_dir}
+    cp -v ${config} ${src_dir}
+    cp -v ${submit_script} ${src_dir}
+    cp -v ${jobScript} ${src_dir}
   fi
 done
 
-export submit_script=$(basename ${submit_script})
+export -f getJsonVal
+export -f read_step_info
+
+export config_name=$(basename ${config})
+export config=${src_dir}/${config_name}
 export jobScript=$(basename ${jobScript})
-export config=$(basename ${config})
-
-export run_transport=${run_steps[transport]}
-export transport_out_dir=${step_out_dirs[transport]}
-export transport_macro=${step_macros[transport]}
-
-export run_digitization=${run_steps[digitization]}
-export digitization_out_dir=${step_out_dirs[digitization]}
-export digitization_macro=${step_macros[digitization]}
- 
-export run_reconstruction=${run_steps[reconstruction]}
-export reconstruction_out_dir=${step_out_dirs[reconstruction]}
-export reconstruction_macro=${step_macros[reconstruction]}
-
-export run_AT=${run_steps[AT]}
-export AT_out_dir=${step_out_dirs[AT]}
-export AT_macro=${step_macros[AT]}
-
 export nEvents=$(getJsonVal "['accessory']['nEvents']")
 
 if [ ${batch} == true ];then
+  jobName=$(getJsonVal "['accessory']['jobName']")
+  jobRange=$(getJsonVal "['accessory']['jobRange']")
+  logDir=$(getJsonVal "['accessory']['logDir']")
   mkdir -pv ${logDir}
   sbatch -J ${jobName} -o ${logDir}/%A_%a.log -a ${jobRange} --export=ALL -- ${jobSript}
 else
-  export SLURM_ARRAY_JOB_ID=jobId
   export SLURM_ARRAY_TASK_ID=taskId
   ./${jobScript}
 fi
--- submit_reco/run_sim_reco.sh
+++ submit_reco/run_sim_reco.sh
@@ -1,44 +1,43 @@
 #!/bin/bash
 
-jobId=${SLURM_ARRAY_JOB_ID}
-taskId=${SLURM_ARRAY_TASK_ID}
-
-if [ ${run_transport} == true ]; then
-  transport_config=transport_${config}
-  cp -v ${transport_out_dir}/${config} ${transport_src_dir}/${transport_config}
-  task_dir=${transport_out_dir}/${taskId}
-  job_log_dir=${transport_out_dir}/log_${jobId}
-  log=${job_log_dir}/${taskId}.tra.log
-  mkdir -pv ${job_log_dir}
-  mkdir -pv ${task_dir}
-  cd ${task_dir}
-  ln -sf ${VMCWORKDIR}/macro/run/.rootrc .
-  echo root -b -l -q "${job_transport_macro}(\"${job_config}\",${nEvents})" &> ${log}
-  gzip -f ${log} 
-  rm .rootrc
-fi
-
-
-#if [ ${run_reconstruction} == true ]; then
-#  input=$(getJsonVal "['reconstruction']['input']")
-#  nTimeSlices=$(getJsonVal "['reconstruction']['nTimeSlices']")
-#  firstTimeSlice=$(getJsonVal "['reconstruction']['firstTimeSlice']")
-#  outputReco=$(getJsonVal "['reconstruction']['output']['path']")
-#  overwriteReco=$(getJsonVal "['reconstruction']['output']['overwrite']")
-#  sEvBuildRaw=$(getJsonVal "['reconstruction']['sEvBuildRaw']")
-#  paramFile=$(getJsonVal "['reconstruction']['paramFile']")
-#  useMC=$(getJsonVal "['reconstruction']['useMC']")
-#  root -b -l -q "${reco_macro}(${input},${nTimeSlices},${firstTimeSlice},${outputReco},${overwriteReco},${sEvBuildRaw},\"${config}\",${paramFile},${useMC})"
-#fi
-#
-#if [ ${run_AT} == true ]; then
-#  traPath=$(getJsonVal "['AT']['traPath']")
-#  rawPath=$(getJsonVal "['AT']['rawPath']")
-#  recPath=$(getJsonVal "['AT']['recPath']")
-#  geoPath=$(getJsonVal "['AT']['geoPath']")
-#  parPath=$(getJsonVal "['AT']['parPath']")
-#  unigenFile=$(getJsonVal "['AT']['unigenFile']")
-#  outputAT=$(getJsonVal "['AT']['output']['path']")
-#  overwriteAT=$(getJsonVal "['AT']['output']['overwrite']")
-#  root -b -l -q "${AT_macro}(${traPath},${rawPath},${recPath},${geoPath},${parPath},${unigenFile},${oututAT},${overwriteAT},\"${config}\",${nEvents})"
-#fi
+steps="transport digitization reconstruction AT"
+for step in ${steps}; do
+  read_step_info
+  if [ ${run} == true ]; then
+    taskId=${SLURM_ARRAY_TASK_ID}
+    config=${src_dir}/${config_name}
+    macro=${src_dir}/${macro_name}
+    out_path=$(getJsonVal "['${step}']['output']['path']")
+    log=${out_path}.${step}.log
+    out_dir=$(dirname ${out_path})
+    mkdir -pv ${out_dir}
+    cd ${out_dir}
+    ln -sf ${VMCWORKDIR}/macro/run/.rootrc .
+    if [ ${step} == reconstruction ]; then
+      input=$(getJsonVal "['reconstruction']['input']")
+      nTimeSlices=$(getJsonVal "['reconstruction']['nTimeSlices']")
+      firstTimeSlice=$(getJsonVal "['reconstruction']['firstTimeSlice']")
+      overwrite=$(getJsonVal "['reconstruction']['output']['overwrite']")
+      sEvBuildRaw=$(getJsonVal "['reconstruction']['sEvBuildRaw']")
+      paramFile=$(getJsonVal "['reconstruction']['paramFile']")
+      useMC=$(getJsonVal "['reconstruction']['useMC']")
+      root -b -l -q "${macro}(\"${input}\",${nTimeSlices},${firstTimeSlice},\"${out_path}\",\
+        ${overwrite},\"${sEvBuildRaw}\",\"${config}\",\"${paramFile}\",${useMC})" &> ${log}
+    elif [ ${step} == AT ]; then
+      traPath=$(getJsonVal "['AT']['traPath']")
+      rawPath=$(getJsonVal "['AT']['rawPath']")
+      recPath=$(getJsonVal "['AT']['recPath']")
+      geoPath=$(getJsonVal "['AT']['geoPath']")
+      parPath=$(getJsonVal "['AT']['parPath']")
+      unigenFile=$(getJsonVal "['AT']['unigenFile']")
+      overwrite=$(getJsonVal "['AT']['output']['overwrite']")
+      root -b -l -q "${macro}(\"${traPath}\",\"${rawPath}\",\"${recPath}\",\"${geoPath}\",\"${parPath}\",\
+	\"${unigenFile}\",\"${out_path}\",${overwrite},\"${config}\",${nEvents})" &> ${log}
+    else 
+      root -b -l -q "${macro}(\"${config}\",${nEvents})" &> ${log}
+    fi
+    gzip -f ${log}
+    rm .rootrc
+    cd -
+  fi
+done
