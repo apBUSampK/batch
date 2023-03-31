229338a0f8726ad5f899599e7b2f4234c7f1e4c9
nepeivodaRS <roman.nepeivoda@hep.lu.se>
2023-03-31 14:18:16 +0200

    EnvDifferentiate

--- submit_urqmd_aamcc/run_gen.sh
+++ submit_urqmd_aamcc/run_gen.sh
@@ -10,9 +10,6 @@ filenum=$SLURM_ARRAY_TASK_ID
 jobDir=${log_dir}/${filenum}
 
 source $root_config
-cd $unigen_path
-source $unigen_path/config/unigenlogin.sh
-#source $mcini_path/macro/config.sh
 
 mkdir -pv ${jobDir}
 cd ${jobDir}
@@ -35,6 +32,11 @@ else
   gunzip $datfile.gz
 fi
 
+(
+cd $unigen_path
+source $unigen_path/config/unigenlogin.sh
+#source $mcini_path/macro/config.sh
+
 which root
 
 # Convert UrQMD to mcini
@@ -42,7 +44,9 @@ echo $LD_LIBRARY_PATH
 $unigen_path/bin/urqmd2u $datfile $rootfile $events_per_file
 #root $source_dir/convertUrQMD.C"(\"$datfile\",\"$rootfile\")"
 gzip -f $datfile
+)
 
+(
 # AfterBurn UrQMD files in AAMCC
 . $out_path/makeup.sh
 cd $outdir_root_aamcc
@@ -53,6 +57,7 @@ echo -e $AAMCCinputString1$rootfile$AAMCCinputString2$filenum > .inFile
 # Process afterburning
 ${aamcc_path}/GRATE < .inFile 1> $outdir_root_aamcc/log
 echo "${filenum} file is afterburned"
+)
 
 [ $remove_logs == "yes" ] && rm -r ${log_dir}/${filenum}
 
