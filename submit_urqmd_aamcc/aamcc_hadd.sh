079510e99cdf87e937cc866d6f292e8f97e81d6b
nepeivodaRS <roman.nepeivoda@hep.lu.se>
2023-03-31 15:52:34 +0200

    updHaddScript

--- submit_urqmd_aamcc/aamcc_hadd.sh
+++ submit_urqmd_aamcc/aamcc_hadd.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+(
+. $out_path/makeup.sh
 source $root_config
-hadd -j -f $outdir_root_aamcc/aamccUrQMDhadd.root $outdir_root_aamcc/urqmd_aamcc_*_mcini_.root >& $outdir_root_aamcc/log_merge.txt
+hadd -j -f $outdir_root_aamcc/aamccUrQMDhadd.root $outdir_root_aamcc/urqmd_aamcc_*_mcini_.root >& $outdir_root_aamcc/logMergeScript
+)
